12569ba0fa8e5d6e25e0ec6b294757a91c552796
bala-amypo <bala@amypo.in>
2025-12-18 08:53:45 +0000

    User commit

--- src/main/java/com/example/demo/controller/ValidationController.java
+++ src/main/java/com/example/demo/controller/ValidationController.java
@@ -1,5 +1,11 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.example.demo.entity.ValidationEntity;
+import com.example.demo.service.ValidationService;
 
 @RestController
 public class ValidationController{
@@ -7,7 +13,7 @@ public class ValidationController{
     @Autowired ValidationService ser;
     
     @PostMapping("/post")
-    public ValidationEntity sendData(@RequestBody ValidationEntity valid){
+    public ValidationEntity sendData(@Valid @RequestBody ValidationEntity valid){
            return ser.postData(valid); 
     }
 }
--- src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
@@ -1,8 +1,17 @@
 package com.example.demo.service.Impl;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Service 
 
 public class ValidationServiceImpl implements ValidationService{
 
+  @Autowired ValidatonRepo validation;
+    
+@Override
+ public ValidationEntity postData(ValidationEntity valid){
+      return validation.save(valid);
+
+ }
 }
