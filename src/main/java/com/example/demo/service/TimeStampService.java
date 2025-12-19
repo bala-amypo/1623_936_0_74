245d0df6cff71c9f940d96f81e1f5939be80738f
bala-amypo <bala@amypo.in>
2025-12-19 08:01:04 +0000

    User commit

--- src/main/java/com/example/demo/service/TimeStampService.java
+++ src/main/java/com/example/demo/service/TimeStampService.java
@@ -3,7 +3,7 @@ package com.example.demo.service; //header file
 import com.example.demo.entity.TimeStampEntity;
 
 
-public interface StService{
-    StudentEntity postData(StudentEntity stu);
+public interface TimeStampService{
+    TimeStampEntity postData(TimeStampEntity time);
 
 }
