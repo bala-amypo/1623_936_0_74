fb0bb313344ccd7dd808f4c8dbd6b84a7b2a2ac9
bala-amypo <bala@amypo.in>
2025-12-24 06:18:15 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentsRepository.java
+++ src/main/java/com/example/demo/repository/StudentsRepository.java
@@ -1,11 +1,11 @@
 package com.example.demo.repository;
-import com.example.data.jpa.repository.Query;
+import com.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.StudentsStructure;
-import com.example.data.repository.query.Param;
+import com.springframework.data.repository.query.Param;
 
 public interface StudentsRepository extends JpaRepository<StudentsStructure, Integer> {
 
-    @Query("SELECT * FROM StudentStructure s WHERE s.studEmail =:email")
-    List<StudentStructure>findEmail(@Param("email") String email)
+    @Query("SELECT * FROM StudentsStructure s WHERE s.studEmail =:email")
+    List<StudentsStructure>findEmail(@Param("email") String email);
 }
