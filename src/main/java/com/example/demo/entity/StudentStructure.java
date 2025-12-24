d14e2beee8a2976491c9c47e5bf4acb349fce884
bala-amypo <bala@amypo.in>
2025-12-24 03:39:43 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/StudentStructure.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentStructure {
+
+    @Id
+    private int studid;
+    private String name;
+    private String email;
+    private double cgpa;
+}
