d8861eb4499903b012aec521bdb408e805b071dc
bala-amypo <bala@amypo.in>
2025-12-24 03:44:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentsStructure.java
+++ src/main/java/com/example/demo/entity/StudentsStructure.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class StudentStructure {
+public class StudentsStructure {
 
     @Id
     private int studid;
