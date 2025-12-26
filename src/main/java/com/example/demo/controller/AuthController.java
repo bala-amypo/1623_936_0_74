640285a98c68f0c28981250bd6264606764dee24
bala-amypo <bala@amypo.in>
2025-12-26 09:07:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.example.demo.entity.ExtraStudent;
-
+import com.example.demo.dto.AuthRequest;
 import com.example.demo.service.ExtraStudentService;
 
 // import org.springframework.security.crypto.password.PasswordEncoder;
