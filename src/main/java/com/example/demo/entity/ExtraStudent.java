6c2c235b371a2d2ee2c626074dbd1397b499970c
bala-amypo <bala@amypo.in>
2025-12-26 07:42:22 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/ExtraStudent.java
@@ -0,0 +1,27 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExtraStudent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Email(message = "Invalid email address")
+    private String email;
+    private String password;
+    private String role;
+
+}
