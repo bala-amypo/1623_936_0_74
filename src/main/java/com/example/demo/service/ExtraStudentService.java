9bd4e5815250f2245463cb195756d4f9a73fb8bf
bala-amypo <bala@amypo.in>
2025-12-26 08:00:23 +0000

    User commit

--- src/main/java/com/example/demo/service/ExtraStudentService.java
+++ src/main/java/com/example/demo/service/ExtraStudentService.java
@@ -5,6 +5,6 @@ import com.example.demo.entity.ExtraStudent;
 import java.util.List;
 
 public interface ExtraStudentService{
-    ExtraStudent postData(ExtraStudent stu);
+    ExtraStudent (ExtraStudent stu);
 
 }
