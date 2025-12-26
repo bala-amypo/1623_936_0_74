e91ace6c7152e4d3ec6794e200e241327beefe6a
bala-amypo <bala@amypo.in>
2025-12-26 09:27:23 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -16,18 +16,22 @@ import com.example.demo.service.ExtraStudentService;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
     @Autowired
     ExtraStudentService ser;
+    @Autowired
+    PasswordEncoder encoder;
 
-    @PostMapping("/register")
-    public ExtraStudent register(@RequestBody ExtraStudent stu) {
+
+    Autowired jwtUtil util;
+
+    @PostMapping("/add")
+    public ExtraStudent addExtraStudent(@RequestBody ExtraStudent stu) {
         return ser.saveExtraStudent(stu);
     }
-@Autowired
-    private PasswordEncoder encoder;
 
- @PostMapping("/login")
-    public String login(@RequestBody AuthRequest request) {
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
         ExtraStudent student = ser.CheckEmail(request.getEmail());
         if (student == null) {
             throw new RuntimeException("User Not Found");
@@ -37,6 +41,39 @@ public class AuthController {
             throw new RuntimeException("Invalid credentials");
         }
 
-        return "Login successful for " + student.getEmail();
-}
+        // return "Login successful for " + student.getEmail();
+
+
+        String token = util.generateToken(
+                student.getEmail(),
+                student.getRole()
+        );
+
+
+        return new AuthResponse(token, student.getRole());
+    }
+    
+//     @Autowired
+//     ExtraStudentService ser;
+
+//     @PostMapping("/register")
+//     public ExtraStudent register(@RequestBody ExtraStudent stu) {
+//         return ser.saveExtraStudent(stu);
+//     }
+// @Autowired
+//     private PasswordEncoder encoder;
+
+//  @PostMapping("/login")
+//     public String login(@RequestBody AuthRequest request) {
+//         ExtraStudent student = ser.CheckEmail(request.getEmail());
+//         if (student == null) {
+//             throw new RuntimeException("User Not Found");
+
+//         }
+//         if (!encoder.matches(request.getPassword(), student.getPassword())) {
+//             throw new RuntimeException("Invalid credentials");
+//         }
+
+//         return "Login successful for " + student.getEmail();
+// }
 }
